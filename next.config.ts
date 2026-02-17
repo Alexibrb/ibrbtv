9cca07b6586e270e1cc23daaddc6b4075ae77883
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2026-02-17 20:44:41 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://caribbe

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'caribbeangospel.tv',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
