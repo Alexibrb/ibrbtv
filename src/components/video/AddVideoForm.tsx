e3836d3b96ead83212f8a4f27bbc68847d36bbe5
Alexandro Alves Pereira <alexandro.ibrb@gmail.com>
2026-02-17 02:26:09 +0000

    Try fixing this error: `Runtime ReferenceError: useState is not defined.

--- src/components/video/AddVideoForm.tsx
+++ src/components/video/AddVideoForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useActionState } from 'react';
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
